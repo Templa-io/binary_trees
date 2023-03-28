544320fc81f3e9745fa5138c706fb6a677f59789
Templa-io <humphreywritter@gmail.com>
2023-03-28 03:13:36 -0700

    update new left node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - create a binary node
- * @parent: pointer to parent node of the node to create
- * @value: value to put in the new node
- *
- * Return: pointer to the new node, or NULL on failure
+ * binary_tree_node - Create a binary node
+ * @parent: node parent
+ * @value: Value of the node
+ * Return: the new node
  */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
@@ -16,11 +16,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
-
-	return (new_node)
+	return (new_node);
 }
