88fef045a23e67089aee2d781e5db60fe21a249f
Templa-io <humphreywritter@gmail.com>
2023-03-31 08:21:21 -0700

    update files

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,14 +1,10 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_insert_left - function that inserts a node as the left-child of another node
- *
- * Description: If parent already has a left-child, the new node must take its place, and the old left-child must be set as the left-child of the new node.
- *
- * @parent: pointer to the node to insert the left-child in
- * @value: value to store in the new node
- *
- * Return: a pointer to the created node, or NULL on failure or if parent is NULL
+ * binary_tree_insert_left - add a node in the left of the parent
+ * if it exists it move down one level and add the new node first
+ * @parent: parent of the specified node
+ * @value: value of the node
+ * Return: NULL if it fails or the new node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
@@ -19,14 +15,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	new_node = binary_tree_node(parent, value)
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-	if (parent -> !=NULL)
+	if (parent->left != NULL)
 	{
-		new_node-> left-parent->left;
+		new_node->left = parent->left;
 		parent->left->parent = new_node;
 	}
 	parent->left = new_node;
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,39 +1,31 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_insert_right - function that inserts a node as the right-child of another node
- * 
- * Description - If parent already has a right-child, the new node must take its place, and the old right-child must be set as the right-child of the new node.
- *
- * @parent - pointer to the node to insert the right-child in
- * @value - value to store in the new node
- *
- * Return - pointer to the created node, or NULL on failure or if parent is NULL
+ * binary_tree_insert_right - add a node in the right of the parent
+ * if it exists it move down one level and add the new node first
+ * @parent: parent of the specified node
+ * @value: value of the node
+ * Return: NULL if it fails or the new node
  */
+
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if (new_node == NULL)
-	{
-		return (NULL)
-	}
-
-	new_node = binary_tree_node(parent, value)
 	if (parent == NULL)
 	{
-		return (NULL)
-	}
-	if (parent->right == NULL)
-	{
-		parent->right = new_node;
-	}
-	else
-	{
-		new_node->right = parent->right;
-		parent->right = new_node;
-		new_node->right->parent = new_node;
+		return (NULL);
 	}
 
-	return (new_node)
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
+	parent->right = new_node;
+	return (new_node);
 }
